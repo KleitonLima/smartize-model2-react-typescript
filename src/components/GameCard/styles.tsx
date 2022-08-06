65bdcb1d2ad9b8aee80be7c348bae8004a616e4f
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-05 22:24:02 -0300

    config: Configurado CardGame e seus estilos inicialmente

--- src/components/GameCard/styles.tsx
+++ src/components/GameCard/styles.tsx
@@ -1,1 +1,32 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Theme } from "../../types/styled-components";
+
+interface CardContainerProps {
+  theme: Theme;
+}
+
+export const CardContainer = styled.div<CardContainerProps>`
+  ${({ theme }) => css`
+    width: 200px;
+    height: 250px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: end;
+    background-color: ${theme.colors.backgroundMenuColor};
+    gap: 6px;
+    border-radius: 16px;
+    font-family: ${theme.constants.bodyFontFamily};
+    color: ${theme.colors.secondaryColor};
+    padding: 8px;
+    position: relative;
+    padding-top: 24px;
+
+    img {
+      width: 150px;
+      height: 150px;
+      position: absolute;
+      top: -32px;
+    }
+  `}
+`;
