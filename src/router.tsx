f737d89cf0dfb3ce4bbf07dfd2074c222d26fb29
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-09 23:22:45 -0300

    feat: rota login adicionada

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" />
     </Routes>
   );
