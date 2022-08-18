43e338eebd90ee3f207c3ba1e483dc496a14de8f
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-18 14:50:07 -0300

    feat: função de checagem de token e redireccionamento dependendo da resposta

--- src/contexts/auth/index.tsx
+++ src/contexts/auth/index.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, ReactNode, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, ReactNode, useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { User } from "../../types";
@@ -35,8 +36,35 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     localStorage.clear();
     navigate("/login");
     setLogged(false);
-    toast.success("Logout bem sucedido!");
   };
+
+  const checkTokenExpiration = () => {
+    const user = JSON.parse(localStorage.getItem("user") || "");
+    const token = localStorage.getItem("token");
+
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    axios
+      .get(`https://smartize-store-back-m4-production.up.railway.app/users/${user.id}`, headers)
+      .then(() => {
+        setLogged(true);
+        navigate("/");
+      })
+      .catch(() => {
+        logout();
+        toast.error("Sessão expirada. Faça login novamente!");
+      });
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) checkTokenExpiration();
+  }, []);
 
   return <AuthContext.Provider value={{ logged, login, logout }}>{children}</AuthContext.Provider>;
 };
