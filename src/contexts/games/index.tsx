c4ab102a99913a10012d5bf340b6d01c7f88daaf
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-22 15:56:14 -0300

    feat: Context do games

---
+++ src/contexts/games/index.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, ReactNode, useState, useEffect } from "react";
+import { api } from "../../services";
+import { Game } from "../../types";
+import { useAuth } from "../auth";
+
+interface GamesProviderProps {
+  children: ReactNode;
+}
+
+interface GamesProviderData {
+  games: Game[];
+}
+
+const GamesContext = createContext<GamesProviderData>({} as GamesProviderData);
+
+export const GamesProvider = ({ children }: GamesProviderProps) => {
+  const { logged } = useAuth();
+  const [games, setGames] = useState<Game[]>([]);
+
+  const handleGetGames = () => {
+    const token = localStorage.getItem("token");
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    api.get("/games", headers).then((res) => setGames(res.data));
+  };
+
+  useEffect(() => {
+    if (logged) handleGetGames();
+  }, [logged]);
+  return <GamesContext.Provider value={{ games }}>{children}</GamesContext.Provider>;
+};
+
+export const useGames = () => useContext(GamesContext);
