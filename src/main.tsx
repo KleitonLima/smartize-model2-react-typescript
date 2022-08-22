8e158778c03c40e4fe0c2dee43ed12d8aa216f9b
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-22 16:07:26 -0300

    fix: Providers transferidos para context Providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-import theme from "./assets/styles/theme";
-import { AuthProvider } from "./contexts/auth";
-import { GamesProvider } from "./contexts/games";
+import Providers from "./contexts";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <GamesProvider>
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
-        </GamesProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
