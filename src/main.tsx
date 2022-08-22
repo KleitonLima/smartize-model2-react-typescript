e6eaaf66f0780ad1c90b70224b795031e49d9704
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-22 15:54:59 -0300

    feat: GamesProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import { ThemeProvider } from "styled-components";
 import App from "./App";
 import theme from "./assets/styles/theme";
 import { AuthProvider } from "./contexts/auth";
+import { GamesProvider } from "./contexts/games";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
+        <GamesProvider>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </GamesProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
