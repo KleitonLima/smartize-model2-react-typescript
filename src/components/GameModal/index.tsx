755a5c1f35a43de4035c83d4c8f640b9186b6724
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-24 20:24:44 -0300

    feat: implementado updateModal

--- src/components/GameModal/index.tsx
+++ src/components/GameModal/index.tsx
@@ -9,17 +9,19 @@ import { useState } from "react";
 import { mockedGenres } from "../../mocks";
 import toast from "react-hot-toast";
 import { useGames } from "../../contexts/games";
+import { Game } from "../../types";
 
 interface GameModalProps {
   handleOpenModal: () => void;
+  game?: Game;
 }
 
-interface NewProductData {
-  name: string;
-  image: string;
-  price: number;
-  description: string;
-  genreId: string;
+interface ProductData {
+  name?: string;
+  image?: string;
+  price?: number;
+  description?: string;
+  genreId?: string;
 }
 
 const newGameSchema = yup.object().shape({
@@ -28,46 +30,64 @@ const newGameSchema = yup.object().shape({
   price: yup.number().required("Preço obrigatório!"),
   description: yup.string().required("Descrição obrigatória!"),
 });
+const updateGameSchema = yup.object().shape({
+  name: yup.string(),
+  image: yup.string().url("Url inválido!"),
+  price: yup.number(),
+  description: yup.string(),
+});
 
-const GameModal = ({ handleOpenModal }: GameModalProps) => {
+const GameModal = ({ handleOpenModal: handleShowModal, game }: GameModalProps) => {
   const { handleGetGames } = useGames();
-  const [genreId, setGenreId] = useState<string>("");
+
+  const [genreId, setGenreId] = useState<string>(game ? game.genreId : "");
+
+  const token = localStorage.getItem("token");
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
   const {
     handleSubmit,
     register,
     formState: { errors },
-  } = useForm<NewProductData>({ resolver: yupResolver(newGameSchema) });
+  } = useForm<ProductData>({ resolver: yupResolver(game ? updateGameSchema : newGameSchema) });
 
-  const handleNewGame = (data: NewProductData) => {
+  const handleNewGame = (data: ProductData) => {
     data.genreId = genreId;
-
-    const token = localStorage.getItem("token");
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
 
     api
       .post("/games", data, headers)
       .then(() => {
         toast.success("Jogo cadastrado com sucesso!");
         handleGetGames();
-        handleOpenModal();
+        handleShowModal();
       })
       .catch(() => {
         toast.error("Selecione uma categoria...");
       });
   };
 
+  const handleUpdateGame = (data: ProductData) => {
+    data.genreId = genreId;
+
+    api.patch(`/games/${game?.id}`, data, headers).then(() => {
+      toast.success("Jogo atualizado com sucesso!");
+      handleGetGames();
+      handleShowModal();
+    });
+  };
+
   return (
     <Styled.ModalOverlay>
-      <Styled.ModalContainer onSubmit={handleSubmit(handleNewGame)}>
-        <h2>Cadastrar jogo</h2>
-        <StyledInput placeholder="Nome" {...register("name")} />
-        <StyledInput placeholder="Imagem" {...register("image")} type="url" />
-        <StyledInput placeholder="Preço" {...register("price")} type="number" step="0.01" />
-        <StyledInput placeholder="Descrição" {...register("description")} />
+      <Styled.ModalContainer onSubmit={handleSubmit(game ? handleUpdateGame : handleNewGame)}>
+        <h2>{game ? "Editando jogo" : "Cadastrar jogo"}</h2>
+        <StyledInput placeholder="Nome" {...register("name")} defaultValue={game ? game.name : ""} />
+        <StyledInput placeholder="Imagem" {...register("image")} defaultValue={game ? game.image : ""} type="url" />
+        <StyledInput placeholder="Preço" {...register("price")} defaultValue={game ? game.price : ""} type="number" step="0.01" />
+        <StyledInput placeholder="Descrição" {...register("description")} defaultValue={game ? game.description : ""} />
         {/* Se deixado vazio o select não envia aviso de erro e faz a requisição com ele vazio,
         gerando erro 400 dizendo que o genreId precisa ser um UUID.
         Temporariamente resolvido com .catch na requisição*/}
@@ -83,7 +103,7 @@ const GameModal = ({ handleOpenModal }: GameModalProps) => {
         </Styled.Select>
         {<ErrorMessage>{errors.name?.message || errors.image?.message || errors.price?.message || errors.description?.message}</ErrorMessage>}
         <div>
-          <Button text="Cancelar" variant="cancel" size="tiny" onClick={handleOpenModal} />
+          <Button text="Cancelar" variant="cancel" size="tiny" onClick={handleShowModal} />
           <Button text="Cadastrar" size="tiny" type="submit" />
         </div>
       </Styled.ModalContainer>
--- src/components/SettingsGameCard/index.tsx
+++ src/components/SettingsGameCard/index.tsx
@@ -1,19 +1,27 @@
 import * as Styled from "./style";
 import { Game } from "../../types";
 import { EditIcon } from "../../assets/icons/index";
+import { Dispatch, SetStateAction } from "react";
 
 interface SettingsGameCardProps {
   game: Game;
+  handleShowModal: () => void;
+  setGame: Dispatch<SetStateAction<Game | undefined>>;
 }
 
-const SettingsGameCard = ({ game }: SettingsGameCardProps) => {
+const SettingsGameCard = ({ game, handleShowModal, setGame }: SettingsGameCardProps) => {
   return (
     <Styled.EditEntityCard>
       <img alt={game.name} src={game.image} />
       <h5>{game.name}</h5>
       <h5>R${game.price}</h5>
       {/* <p>{game.description}</p> */}
-      <Styled.SettingsGameCardButton>
+      <Styled.SettingsGameCardButton
+        onClick={() => {
+          setGame(game);
+          handleShowModal();
+        }}
+      >
         <EditIcon />
         Editar jogo
       </Styled.SettingsGameCardButton>
