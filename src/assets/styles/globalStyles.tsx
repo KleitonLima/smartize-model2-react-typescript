3d118278cadf1c9e545ae76d77c92458e13f74e6
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-26 22:56:51 -0300

    feat: novas styles

--- src/assets/styles/globalStyles.tsx
+++ src/assets/styles/globalStyles.tsx
@@ -49,3 +49,65 @@ export const ModalOverlay = styled.div`
     background-color: #0009;
   `}
 `;
+
+export const SettingsContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    background-color: ${theme.colors.backgroundColor};
+  `}
+`;
+
+export const SettingsSelectedContainer = styled.div`
+  ${({ theme }) => css`
+    width: calc(75% - 100px);
+    height: calc(100% - 68px);
+    display: flex;
+    flex-direction: column;
+    background-color: ${theme.colors.backgroundMenuColor};
+    margin: 68px 16px 0px 0px;
+    border-radius: 8px 8px 0px 0px;
+    padding: 16px;
+    box-sizing: border-box;
+    gap: 16px;
+
+    div {
+      display: flex;
+    }
+  `}
+`;
+
+export const SelectedContentsContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-wrap: wrap;
+    overflow-y: scroll;
+    gap: 16px;
+  `}
+`;
+
+export const AddEntityCard = styled.button`
+  ${({ theme }) => css`
+    width: 200px;
+    height: 250px;
+    border: 2px dashed ${theme.colors.primaryColor};
+    border-radius: 4px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    color: ${theme.colors.primaryColor};
+    text-shadow: 0px 0px 5px ${theme.colors.textPrimaryColor};
+
+    :hover {
+      transform: scale(0.99);
+      background-color: ${theme.colors.backgroundColor};
+      color: ${theme.colors.textPrimaryColor};
+      transition: all 0.2s;
+    }
+  `}
+`;
