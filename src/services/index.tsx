c0e0b2b90b5063108c5305d0c28431fd160e100e
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-29 15:03:16 -0300

    refactor: link do back

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://smartize-store-back-m4-production.up.railway.app" });
+export const api = axios.create({ baseURL: "https://smartize-store-back-m4-production.up.railway.app" });
