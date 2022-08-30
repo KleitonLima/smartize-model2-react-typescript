d994bb7e83792c153ca9e395dc0696ec58b19ef3
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-30 20:38:42 -0300

    feat: lógica para favoritar e desfavoritar jogos, botão favoritar

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,13 +1,56 @@
-import { Game } from "../../types/index.js";
+import toast from "react-hot-toast";
+import { FavoriteIcon } from "../../assets/icons/index.js";
+import { api } from "../../services/index.js";
+import { Favorite, Game } from "../../types/index.js";
 import * as Styled from "./styles";
 
 interface GameCardProps {
   game: Game;
+  isFavoriteList: boolean;
+  handleGetFavorites: () => void;
 }
 
-const GameCard = ({ game }: GameCardProps) => {
+const GameCard = ({ game, isFavoriteList, handleGetFavorites }: GameCardProps) => {
+  const token = localStorage.getItem("token");
+  const user = JSON.parse(localStorage.getItem("user") || "");
+
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const data = {
+    userId: user.id,
+    gameId: game.id,
+  };
+
+  const handleSetFavorite = () => {
+    api
+      .post("/favorites", data, headers)
+      .then(() => {
+        handleGetFavorites();
+        toast.success("Jogo favoritado!");
+      })
+      .catch(() => toast.error("Produto já favoritado"));
+  };
+
+  const handleRemoveFavorite = async () => {
+    const res = await api.get<Favorite[]>(`/favorites/user/${user.id}`, headers);
+
+    const favoriteId = res.data.filter((elem) => elem.gameId === game.id).find((elem) => elem.userId === user.id)?.id;
+
+    api.delete(`/favorites/${favoriteId}`, headers).then(() => {
+      handleGetFavorites();
+      toast.success("Removido dos favoritos");
+    });
+  };
+
   return (
     <Styled.CardContainer>
+      <button onClick={isFavoriteList ? handleRemoveFavorite : handleSetFavorite}>
+        <FavoriteIcon />
+      </button>
       <img alt={game.name} src={game.image} />
       <h3>{game.name}</h3>
       <h3>R${game.price}</h3>
