e3cb4fe95083b44b2f351e54a8afddd4b9190ca8
Kleiton Lima <kleiton.mini@gmail.com>
2022-08-30 17:58:27 -0300

    feat: lógica de filtro e exibição dos favoritos adicionados

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,20 +2,52 @@ import OrderDetails from "../../components/OrderDetails";
 import GamesList from "../../components/GamesList";
 import Menu from "../../components/Menu";
 import { SearchIcon } from "../../assets/icons";
-import { Game, Genre } from "../../types";
+import { Favorite, Game, Genre } from "../../types";
 import * as Styled from "./styles";
 import { DateTime } from "luxon";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useGames } from "../../contexts/games";
 import { useGenres } from "../../contexts/genres";
+import { api } from "../../services";
 
 const Home = () => {
   const { games } = useGames();
   const { genres } = useGenres();
-  const [selectedGenre, setSelectedGenre] = useState<Genre>(genres[0] || ({} as Genre));
+  const [selectedGenre, setSelectedGenre] = useState<Genre>(({} as Genre) || genres[0]);
+  const [isFavoriteList, setIsFavoriteList] = useState<boolean>(false);
+  const [userFavorites, setUserFavorites] = useState<Game[]>([]);
+
   const filteredGames: Game[] = games.filter((elem) => elem.genreId === selectedGenre.id);
+
   const actualDate = DateTime.now();
   const formatedDate = `${actualDate.weekdayShort}, ${actualDate.day} ${actualDate.monthLong} ${actualDate.year}`;
+
+  const handleGetFavorites = async () => {
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user") || "");
+
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const res = await api.get<Favorite[]>(`/favorites/user/${user.id}`, headers);
+
+    const favoritesIds: string[] = res.data.map((elem) => elem.gameId);
+
+    const favoritesList: Game[] = games.filter((elem) => {
+      return favoritesIds.includes(elem.id);
+    });
+
+    console.log(favoritesIds);
+    console.log(favoritesList);
+    setUserFavorites(favoritesList);
+  };
+
+  useEffect(() => {
+    handleGetFavorites();
+  }, [games]);
 
   return (
     <Styled.HomeContainer>
@@ -40,11 +72,27 @@ const Home = () => {
             <Styled.GenreSelectButton>Todos</Styled.GenreSelectButton>
             {genres.map((elem) => {
               return (
-                <Styled.GenreSelectButton key={elem.name.toString()} active={elem.name === selectedGenre.name} onClick={() => setSelectedGenre(elem)}>
+                <Styled.GenreSelectButton
+                  key={elem.name.toString()}
+                  active={elem.name === selectedGenre.name}
+                  onClick={() => {
+                    setIsFavoriteList(false);
+                    setSelectedGenre(elem);
+                  }}
+                >
                   {elem.name}
                 </Styled.GenreSelectButton>
               );
             })}
+            <Styled.GenreSelectButton
+              active={isFavoriteList}
+              onClick={() => {
+                setIsFavoriteList(true);
+                setSelectedGenre({} as Genre);
+              }}
+            >
+              Favoritos
+            </Styled.GenreSelectButton>
           </Styled.GenreBar>
           <Styled.GamesHeaderContainer>
             <div>
@@ -59,7 +107,7 @@ const Home = () => {
               </Styled.GamesOrderSelect>
             </div>
           </Styled.GamesHeaderContainer>
-          <GamesList list={filteredGames} />
+          <GamesList list={isFavoriteList ? userFavorites : filteredGames} />
         </section>
       </Styled.HomeContentContainer>
       <OrderDetails />
