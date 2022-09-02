c032ccb500c0e53d0495e8e18a9807404bcd2579
Kleiton Lima <kleiton.mini@gmail.com>
2022-09-01 23:10:31 -0300

    refactor: mudança de cores e reposicionamento

--- src/assets/styles/colors.tsx
+++ src/assets/styles/colors.tsx
@@ -1,11 +1,9 @@
 export const colors = {
   primaryColor: "#0511f2",
-  secondaryColor: "#ff0000",
-  secondaryColorOpacity: "#ff0000bc",
-  primaryColorOpacity: "#0511f2c6",
-  textSecondaryColor: "#000000",
-  backgroundColor: "#5c82f2",
+  secondaryColor: "#f00",
   textPrimaryColor: "#fff",
+  textSecondaryColor: "#000",
+  backgroundColor: "#5c82f2",
   backgroundMenuColor: "#b3c4f8",
   transparent: '#fff0',
 };
